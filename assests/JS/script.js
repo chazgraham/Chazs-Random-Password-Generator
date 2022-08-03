a168053298f247abe911fe20a377503f48b9d98f
chaz Graham <chazhg80.cg@gmail.com>
2022-08-03 09:45:34 -0500

    added the remaining window prompts and lists

--- assests/JS/script.js
+++ assests/JS/script.js
@@ -10,7 +10,17 @@ function generatePassword() {
     return;
   }
 
-  console.log(userinput)
+  var numbers = window.confirm("Would you like your password to include numbers?");
+  var symbols = window.confirm("Would you like your password to include symbols?");
+  var lowerCase = window.confirm("Would you like your password to include lowercase letters?");
+  var upperCase = window.confirm("Would you like your password to include uppercase letters?");
+  
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8 ", "9"];
+  var symbolsList = ["!", "@", "#", "$", "%", "?", "&", "*"];
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  
 }
 
 
