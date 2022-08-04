d742b95a67b1707f31e7ef59a78903fba010b523
chaz Graham <chazhg80.cg@gmail.com>
2022-08-03 22:06:52 -0500

    removed space after the 8 in the number list array

--- assests/JS/script.js
+++ assests/JS/script.js
@@ -21,7 +21,7 @@ function generatePassword() {
   var upperCase = window.confirm("Would you like your password to include uppercase letters?");
   
   // arrays of the list of characters that fit with the prompts above 
-  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8 ", "9"];
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var symbolsList = ["!", "@", "#", "$", "%", "?", "&", "*"];
   var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
